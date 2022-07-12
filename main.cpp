e5b2b3db1e6707a9db3657b12969ea75347438bd
Maxim Mikhailov <mos_gor@list.ru>
2022-07-13 02:58:30 +0700

    Second

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
+
+std::string first_part(std::string str){
+    std::string library = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-.!#$%&'*+-/=?^_`{|}~";
+}
+
+std::string second_part(std::string str){
+    std::string library = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-.";
+}
+
+bool isValid(std::string email){
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Is your email valid?" << std::endl;
+    std::cout << "Input your email: ";
+    std::string email;
+    std::cin >> email;
+    std::cout << isValid(email);
     return 0;
 }
